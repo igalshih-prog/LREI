9a5b85efcf24cd207fd46596ec157a400711afda
igalshih-prog <igalshih@gmail.com>
2026-09-18 09:20:07 +0300

    Measure Pro number signal shrinkage

--- tests/lottery/test_pro_scoring_ablation.py
+++ tests/lottery/test_pro_scoring_ablation.py
@@ -21,6 +21,9 @@ def test_pro_scoring_ablation_smoke():
         "rank_blend": ProRecommendationEngine(ProConfig(candidate_count=300, use_rank_normalization=True, use_ewma=False, affinity_prior_strength=0.0)),
         "rank_plus_ewma": ProRecommendationEngine(ProConfig(candidate_count=300, use_rank_normalization=True, use_ewma=True, affinity_prior_strength=0.0)),
         "rank_plus_bayesian_affinity": ProRecommendationEngine(ProConfig(candidate_count=300, use_rank_normalization=True, use_ewma=False, affinity_prior_strength=40.0)),
+        "signal_75": ProRecommendationEngine(ProConfig(candidate_count=300, number_signal_strength=0.75)),
+        "signal_50": ProRecommendationEngine(ProConfig(candidate_count=300, number_signal_strength=0.50)),
+        "signal_25": ProRecommendationEngine(ProConfig(candidate_count=300, number_signal_strength=0.25)),
     }
 
     results = {name: [] for name in variants}
